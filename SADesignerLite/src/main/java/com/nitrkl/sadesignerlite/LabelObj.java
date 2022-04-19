3bedba9d21b1df9b46c51bf07fb6f8a7b68015f3
Leo Raphael Rodrigues <leoraphaelro@hotmail.com>
2022-04-19 16:38:14 +0400

    Added Label Functionality

--- SADesignerLite/src/main/java/com/nitrkl/sadesignerlite/LabelObj.java
+++ SADesignerLite/src/main/java/com/nitrkl/sadesignerlite/LabelObj.java
@@ -4,6 +4,11 @@
  */
 package com.nitrkl.sadesignerlite;
 
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.Point;
+import java.awt.geom.Rectangle2D;
+
 /**
  *
  * @author hp
@@ -11,4 +16,30 @@ package com.nitrkl.sadesignerlite;
 class LabelObj extends ShapeObj {
     int labelCount;
     String text;
+    int x, y;
+    LabelObj(Point t){
+        Name = "Hello WOrkld";
+        type = Type.Label;
+        x = t.x;
+        y = t.y;
+        position = new Positions(x, y, x, y);
+    }
+    
+     public void draw(Graphics g){        
+        //name
+        if (Name != null) {
+            FontMetrics fm = g.getFontMetrics();
+            Rectangle2D strb = fm.getStringBounds(Name, g);
+            System.out.println(Name);
+            position.topLeft.x = (int) (x - strb.getWidth()/2); 
+            position.topLeft.y = (int) (y + strb.getHeight()/2);
+            position.bottomRight.x = (int) (x + strb.getWidth()/2);
+            position.bottomRight.y = (int) (y - strb.getHeight()/2);
+            g.drawString (Name,
+                ((position.topLeft.x + position.bottomRight.x)/2 - fm.stringWidth(Name)/2),
+                (int) ((position.topLeft.y + position.bottomRight.y)/2 + strb.getHeight()));
+        }
+        
+    }
+    
 }
